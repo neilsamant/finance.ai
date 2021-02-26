e9f76fa3e33cf9e07c392578e287368dcb4fe97d
Revant Adlakha <36800932+revantad@users.noreply.github.com>
2021-02-26 15:41:24 -0500

    Delete data_aq.py

--- finviz_data/data_aq.py
+++
@@ -1,7 +0,0 @@
-import finviz as fv
-filters = ['exch_nasd', 'idx_sp500']
-stock_list = fv.Screener(filters = filters, table = 'Performance', order = 'price')
-print(stock_list)
-
-stock = fv.get_stock('AAPL')
-print(stock['Price'])
